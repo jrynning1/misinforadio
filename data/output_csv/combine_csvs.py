5a72495050f0cd417b24d6ed407aafa621d3e13d
Jordan Rynning <jrynning@stanford.edu>
2024-07-25 17:21:23 -0400

    add combine output csv script

---
+++ data/output_csv/combine_csvs.py
@@ -0,0 +1,19 @@
+# Run this script to combine all output CSV files in this folder.
+
+import pandas as pd
+from pathlib import Path
+import os
+
+output_csv_filenames = Path().cwd()
+
+output_csv_filenames = os.listdir(output_csv_filenames)
+
+df = pd.DataFrame()
+
+for file in output_csv_filenames:
+    temp_df = pd.read_csv(file)
+    pd.concat(df, temp_df)
+
+df = df.sorted('similarity_value', ascending=False)
+
+df.to_csv('combined_potential_misinformation.csv')

a92524946d8ad1142ab4391b8a8adcaa558e6443
Jordan Rynning <jrynning@stanford.edu>
2024-09-11 21:34:11 -0400

    clean

--- scripts/1_run_all.sh
+++ scripts/1_run_all.sh
@@ -3,7 +3,7 @@
 parentdir=$(dirname `pwd`)
 if [ ! -e "$parentdir"/"data/factchecked_statements/embedded_false_statements.csv" ]
 then
-    python 1b_fact_check_insights_embeddings_2_years_claims_only.py
+    python 0a_fact_check_insights_embeddings_filtered.py
 else
     echo "Fact-checks previously embedded"
 fi && python 2_convert_input_to_aac.py && python 3a_transcribe_local.py && python 4_transcript_embeddings.py && python 5_embeddings_comparisons.py

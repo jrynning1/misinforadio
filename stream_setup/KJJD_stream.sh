74c3308ce8f7fadc703f8d7893f8cb4673e4c404
Jordan Rynning <jrynning@stanford.edu>
2024-10-18 10:30:14 -0700

    change vlc recordings to cvlc

--- stream_setup/KJJD_stream.sh
+++ stream_setup/KJJD_stream.sh
@@ -10,4 +10,4 @@ timestamp=$(date +%y%m%d%H%M%S)
 
 dest=${DIR}${station}_${timestamp}.mp3
 
-vlc $URL --sout="#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100,scodec=none}:file{mux=mp3,dst=$dest,no-overwrite}" --no-sout-all --sout-keep --run-time=3600 --play-and-exit
+cvlc $URL --sout="#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100,scodec=none}:file{mux=mp3,dst=$dest,no-overwrite}" --no-sout-all --sout-keep --run-time=3600 --play-and-exit

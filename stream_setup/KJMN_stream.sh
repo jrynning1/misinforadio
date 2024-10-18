7446377864ff1e884f03207d30b5e9728c7d6a9b
Jordan Rynning <jrynning@stanford.edu>
2024-10-18 15:53:50 -0700

    back to vlc because some recordings failed to stop after one hour

--- stream_setup/KJMN_stream.sh
+++ stream_setup/KJMN_stream.sh
@@ -10,5 +10,5 @@ timestamp=$(date +%y%m%d%H%M%S)
 
 dest=${DIR}${station}_${timestamp}.mp3
 
-cvlc $URL --sout="#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100,scodec=none}:file{mux=mp3,dst=$dest,no-overwrite}" --no-sout-all --sout-keep --run-time=3600 --play-and-exit
+vlc $URL --sout="#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100,scodec=none}:file{mux=mp3,dst=$dest,no-overwrite}" --no-sout-all --sout-keep --run-time=3600 --play-and-exit
 

9698b734ae063409ae9edbf0ab9e10b2dfd4b705
jrynning1 <162841865+jrynning1@users.noreply.github.com>
2024-10-17 17:27:46 -0700

    Add stream template

---
+++ http_stream_template.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+station="<Insert station callsign>"
+
+
+URL="<Insert stream URL>"
+
+DIR="<Insert save location for recording>"
+
+while :
+do
+	timestamp=$(date +%y%m%d%H%M%S);
+	dest=${DIR}${station}_${timestamp}.mp3
+	vlc $URL --sout="#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100,scodec=none}:file{mux=mp3,dst=$dest,no-overwrite}" --no-sout-all --sout-keep --run-time=3630 --play-and-exit;
+done
